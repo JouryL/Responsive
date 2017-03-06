c081dc2e186552942542773027e976d64186d4d2
JouryL <jourylint@hotmail.com>
2017-03-06 21:42:49 +0100

    Add files via upload

---
+++ javascript/mapsContact.js
@@ -0,0 +1,26 @@
+// Locatie van Jomarka
+var huis = new google.maps.LatLng(51.245745, 5.826733);
+
+
+function initialize()
+{
+	// Start gegens invoeren van google maps
+	var mapProp = 
+	{
+		center:huis,
+		zoom:16,
+		mapTypeId:google.maps.MapTypeId.HYBRID
+  	};
+
+  	var map = new google.maps.Map(document.getElementById("googleMapsContact"),mapProp);
+  	
+  	// Positie van locatie aangeven op de map
+  	var marker = new google.maps.Marker(
+  	{
+  		position:huis,
+  	});
+  	marker.setMap(map);
+ 
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
